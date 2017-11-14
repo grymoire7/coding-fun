c07ec8ad278c61435a805330f5a7c96f1efbf421
Tracy Atteberry <tracy@magicbydesign.com>
2017-11-13 21:00:46 -0600

    add implmentations for war and spec

--- ruby/spec/war_spec.rb
+++ ruby/spec/war_spec.rb
@@ -1,14 +1,108 @@
 require 'spec_helper'
 
 RSpec.describe War do
-  before do
-    @war = War.new('Alice', 'Bob')
+  let(:names) { %w[Alice Bob] }
+  let!(:war) { War.new(*names) }
+
+  describe '#deal' do
+    before :each do
+      war.deal
+    end
+    it 'should deal 52 cards equally' do
+      total = 0
+      war.hands.each_value do |hand|
+        expect(hand.size).to eq(26)
+        total += hand.size
+      end
+      expect(total).to eq(52)
+    end
+    it 'deals all cards exactly once' do
+      deck = []
+      war.hands.each_value do |hand|
+        hand.each do |card|
+          deck << card.value
+        end
+      end
+      expect(deck.size).to eq(52)
+      (2..14).each do |value|
+        expect(deck.count(value)).to eq(4)
+      end
+    end
   end
 
   describe '#new' do
     it 'takes an array of player names and returns a War object' do
-      expect(@war).to be_an_instance_of War
+      expect(war).to be_an_instance_of War
+    end
+    it 'raises an error with less than two players' do
+      expect { War.new }.to raise_error(RuntimeError, /two players/)
+    end
+    it 'creates an array of players with names and cards' do
+      expect(war.players.size).to eq(2)
+    end
+  end
+
+  describe '#play_into_pot' do
+    before :each do
+      war.deal
+    end
+    it 'adds one card per player' do
+      war.play_into_pot
+      expect(war.pot.size).to eq(2)
+    end
+    it 'removes a card from each player' do
+      war.play_into_pot
+      war.hands.each_value do |hand|
+        expect(hand.size).to eq(25)
+      end
+    end
+  end
+
+  describe '#award_pot_to' do
+    before :each do
+      war.deal
+      war.play_into_pot
+      winner, = war.hands.first
+      war.award_pot_to winner
+    end
+    it 'removes all cards from the pot' do
+      expect(war.pot).to be_empty
+    end
+  end
+
+  describe '#remove_empty_hands' do
+    it 'removes empty hands' do
+      war.deal
+      war.hands.values[0].clear
+      war.remove_empty_hands
+      expect(war.hands.size).to eq(1)
+    end
+  end
+
+  describe '#play' do
+    it 'should return a winner' do
+      expect(war.play).to be_an_instance_of Array
+    end
+    it 'should not be biased' do
+      stats = StatsCollector.new(war)
+      stats.play(1000)
+      percent_win = stats.results.first[1][:percent_win]
+      expect(percent_win).to be > 45
+      expect(percent_win).to be < 55
+    end
+  end
+
+  describe 'Card' do
+    describe '#new' do
+      it 'takes an owner and value and returns a Card object' do
+        expect(Card.new('owner', 5)).to be_an_instance_of Card
+      end
+      it 'raises an error with less than two params' do
+        expect { Card.new }.to raise_error(ArgumentError)
+      end
+      it 'raises an error bad params' do
+        expect { Card.new(5, 'bob') }.to raise_error(ArgumentError)
+      end
     end
   end
 end
-
--- ruby/war.rb
+++ ruby/war.rb
@@ -1,9 +1,163 @@
 #!/usr/bin/env ruby
 
-class Deck; end
-class War
-  def initialize(*players)
-    @players = players
+class Pot < Array; end
+class Cards < Array; end
+
+class Card
+  attr_accessor :owner, :value
+
+  def initialize(owner, value)
+    @owner = String(owner)
+    @value = Integer(value)
+  end
+
+  def to_s; "#{owner}, #{value}"; end
+  def to_str; to_s; end
+end
+
+class StatsCollector
+  attr_accessor :data, :players, :results
+
+  def initialize(game)
+    @game = game
+    @players = game.players
+    @data = []
+  end
+
+  def add_win(player, rounds)
+    @data << { player => rounds }
+  end
+
+  def play(num_games)
+    num_games.times do
+      winner, rounds = @game.play
+      add_win(winner, rounds)
+    end
+    collect
+  end
+
+  def collect
+    @results = {}
+    total = @data.size
+    @players.each do |player|
+      set = @data.select { |datum| datum.key? player }
+      percent_win = set.size.fdiv(total) * 100
+      rounds = set.inject(0) { |sum, datum| sum + datum[player] }
+      avg_rounds = rounds.fdiv(set.size)
+      @results[player] = {
+        wins: set.size,
+        percent_win: percent_win,
+        avg_rounds: avg_rounds
+      }
+    end
+  end
+
+  def report
+    puts "In #{@data.size} games..."
+    @results.each do |player, result|
+      print "#{player} won #{result[:wins]} times. "
+      print format('(%.02f%%) in an average', result[:percent_win])
+      puts format(' of %.02f rounds.', result[:avg_rounds])
+    end
   end
 end
 
+class War
+  attr_accessor :players, :hands, :pot
+  DEBUG = false
+
+  def debug(msg, obj = nil)
+    msg = "== Debug: #{msg}" unless msg.class != String
+    warn msg if DEBUG
+    warn obj if DEBUG && !obj.nil?
+  end
+
+  def initialize(*player_names)
+    raise 'war needs at least two players' if player_names.size < 2
+    @players = player_names
+    @pot = Pot.new
+    @hands = {}
+  end
+
+  def reset
+    @pot.clear
+    @hands.clear
+    @players.each do |player|
+      @hands[player] = Cards.new
+    end
+  end
+
+  def deal
+    reset
+    deck_values = []
+    4.times { deck_values += (2..14).to_a }
+    deck_values.shuffle!
+    i = 0
+    deck_values.each do |value|
+      owner = @players[i % @players.size]
+      @hands[owner] << Card.new(owner, value)
+      i += 1
+    end
+  end
+
+  def play_into_pot
+    @hands.each_value do |cards|
+      @pot << cards.shift unless cards.empty?
+    end
+  end
+
+  def find_round_winner
+    # TODO: handle ties by playing more hands
+    # TODO: right now it chooses randomly
+    # winners = @pot.group_by { |card| card.value }.max.last
+    winners = @pot.group_by(&:value).max.last
+    winner = winners[Random.rand(winners.size).round].owner
+    winner
+  end
+
+  def award_pot_to(player)
+    @pot.each do |card|
+      card.owner = player
+    end
+    @hands[player] += @pot
+    @pot.clear
+  end
+
+  def remove_empty_hands
+    @hands.delete_if { |_, hand| hand.empty? }
+  end
+
+  def play_round
+    play_into_pot
+    winner = find_round_winner
+    debug "round winner = #{winner}"
+    award_pot_to winner
+    @hands.each do |owner, hand|
+      hand.shuffle! # shuffle to prevent cylces
+      debug ">>> #{owner} has #{hand.size} cards"
+    end
+    remove_empty_hands
+  end
+
+  def play
+    deal
+    rounds = 0
+    while @hands.size > 1
+      play_round
+      rounds += 1
+    end
+    winner, = @hands.first
+    [winner, rounds]
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  game = War.new('Alice', 'Bob')
+  winner, rounds = game.play
+  puts "#{winner} wins in #{rounds} rounds."
+
+  # See if there's a bias over time.
+  stats = StatsCollector.new(game)
+  stats.play(1000)
+  stats.report
+end
