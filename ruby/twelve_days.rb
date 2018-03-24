c91f02aab982facde087ebb08327d56ea80b66d9
Tracy Atteberry <tracy@magicbydesign.com>
2018-03-24 14:41:52 -0500

    extract line duplication

--- ruby/twelve_days.rb
+++ ruby/twelve_days.rb
@@ -2,103 +2,23 @@
 
 class TwelveDays
   ORDINALS = %w(zeroth first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth)
+  LINES = [
+        "Twelve drummers drumming",
+        "Eleven pipers piping",
+        "Ten lords a-leaping",
+        "Nine ladies dancing",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five gold rings",
+        "Four colly birds",
+        "Three french hens",
+        "Two turtle doves, and",
+        "A partridge in a pear tree" ]
 
   def verse(i)
     "On the %s day of christmas my true love sent to me\n" % ORDINALS[i] +
-    begin
-      case i
-      when 1
-        "A partridge in a pear tree\n"
-      when 2
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 3
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 4
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 5
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 6
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 7
-        "Seven swans a-swimming\n" +
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 8
-        "Eight maids a-milking\n" +
-        "Seven swans a-swimming\n" +
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 9
-        "Nine ladies dancing\n" +
-        "Eight maids a-milking\n" +
-        "Seven swans a-swimming\n" +
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 10
-        "Ten lords a-leaping\n" +
-        "Nine ladies dancing\n" +
-        "Eight maids a-milking\n" +
-        "Seven swans a-swimming\n" +
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 11
-        "Eleven pipers piping\n" +
-        "Ten lords a-leaping\n" +
-        "Nine ladies dancing\n" +
-        "Eight maids a-milking\n" +
-        "Seven swans a-swimming\n" +
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      when 12
-        "Twelve drummers drumming\n" +
-        "Eleven pipers piping\n" +
-        "Ten lords a-leaping\n" +
-        "Nine ladies dancing\n" +
-        "Eight maids a-milking\n" +
-        "Seven swans a-swimming\n" +
-        "Six geese a-laying\n" +
-        "Five gold rings\n" +
-        "Four colly birds\n" +
-        "Three french hens\n" +
-        "Two turtle doves, and\n" +
-        "A partridge in a pear tree\n"
-      end
-    end
+      LINES.last(i).join("\n") + "\n"
   end
 
   def recite
