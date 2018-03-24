1b2706e18cff79a5b04a3305aa7ab37487507ed3
Tracy Atteberry <tracy@magicbydesign.com>
2018-03-24 14:33:22 -0500

    pull out the ordinals

--- ruby/twelve_days.rb
+++ ruby/twelve_days.rb
@@ -1,112 +1,103 @@
 #!/usr/bin/env ruby
 
 class TwelveDays
-  ORDINALS = %w(zeroth first second third fourth sixth seventh eighth ninth tenth eleventh twelfth)
+  ORDINALS = %w(zeroth first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth)
 
   def verse(i)
-    case i
-    when 1
-      "On the %s day of christmas my true love sent to me\n" % "first" +
-      "A partridge in a pear tree\n"
-    when 2
-      "On the %s day of christmas my true love sent to me\n" % "second" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 3
-      "On the %s day of christmas my true love sent to me\n" % "third" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 4
-      "On the %s day of christmas my true love sent to me\n" % "fourth" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 5
-      "On the %s day of christmas my true love sent to me\n" % "fifth" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 6
-      "On the %s day of christmas my true love sent to me\n" % "sixth" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 7
-      "On the %s day of christmas my true love sent to me\n" % "seventh" +
-      "Seven swans a-swimming\n" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 8
-      "On the %s day of christmas my true love sent to me\n" % "eighth" +
-      "Eight maids a-milking\n" +
-      "Seven swans a-swimming\n" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 9
-      "On the %s day of christmas my true love sent to me\n" % "ninth" +
-      "Nine ladies dancing\n" +
-      "Eight maids a-milking\n" +
-      "Seven swans a-swimming\n" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 10
-      "On the %s day of christmas my true love sent to me\n" % "tenth" +
-      "Ten lords a-leaping\n" +
-      "Nine ladies dancing\n" +
-      "Eight maids a-milking\n" +
-      "Seven swans a-swimming\n" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 11
-      "On the %s day of christmas my true love sent to me\n" % "eleventh" +
-      "Eleven pipers piping\n" +
-      "Ten lords a-leaping\n" +
-      "Nine ladies dancing\n" +
-      "Eight maids a-milking\n" +
-      "Seven swans a-swimming\n" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
-    when 12
-      "On the %s day of christmas my true love sent to me\n" % "twelfth" +
-      "Twelve drummers drumming\n" +
-      "Eleven pipers piping\n" +
-      "Ten lords a-leaping\n" +
-      "Nine ladies dancing\n" +
-      "Eight maids a-milking\n" +
-      "Seven swans a-swimming\n" +
-      "Six geese a-laying\n" +
-      "Five gold rings\n" +
-      "Four colly birds\n" +
-      "Three french hens\n" +
-      "Two turtle doves, and\n" +
-      "A partridge in a pear tree\n"
+    "On the %s day of christmas my true love sent to me\n" % ORDINALS[i] +
+    begin
+      case i
+      when 1
+        "A partridge in a pear tree\n"
+      when 2
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 3
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 4
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 5
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 6
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 7
+        "Seven swans a-swimming\n" +
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 8
+        "Eight maids a-milking\n" +
+        "Seven swans a-swimming\n" +
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 9
+        "Nine ladies dancing\n" +
+        "Eight maids a-milking\n" +
+        "Seven swans a-swimming\n" +
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 10
+        "Ten lords a-leaping\n" +
+        "Nine ladies dancing\n" +
+        "Eight maids a-milking\n" +
+        "Seven swans a-swimming\n" +
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 11
+        "Eleven pipers piping\n" +
+        "Ten lords a-leaping\n" +
+        "Nine ladies dancing\n" +
+        "Eight maids a-milking\n" +
+        "Seven swans a-swimming\n" +
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      when 12
+        "Twelve drummers drumming\n" +
+        "Eleven pipers piping\n" +
+        "Ten lords a-leaping\n" +
+        "Nine ladies dancing\n" +
+        "Eight maids a-milking\n" +
+        "Seven swans a-swimming\n" +
+        "Six geese a-laying\n" +
+        "Five gold rings\n" +
+        "Four colly birds\n" +
+        "Three french hens\n" +
+        "Two turtle doves, and\n" +
+        "A partridge in a pear tree\n"
+      end
     end
   end
 
