ea665ab1c08d820fea19881ce8f272069bb032e8
Tracy Atteberry <tracy@magicbydesign.com>
2018-03-24 14:10:30 -0500

    expand to all verses and add verse

--- ruby/twelve_days.rb
+++ ruby/twelve_days.rb
@@ -1,22 +1,117 @@
 #!/usr/bin/env ruby
 
 class TwelveDays
+  def verse(i)
+    case i
+    when 1
+      "On the first day of christmas my true love sent to me\n" +
+      "A partridge in a pear tree\n"
+    when 2
+      "On the second day of christmas my true love sent to me\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 3
+      "On the third day of christmas my true love sent to me\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 4
+      "On the fourth day of christmas my true love sent to me\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 5
+      "On the fifth day of christmas my true love sent to me\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 6
+      "On the sixth day of christmas my true love sent to me\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 7
+      "On the seventh day of christmas my true love sent to me\n" +
+      "Seven swans a-swimming\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 8
+      "On the eighth day of christmas my true love sent to me\n" +
+      "Eight maids a-milking\n" +
+      "Seven swans a-swimming\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 9
+      "On the ninth day of christmas my true love sent to me\n" +
+      "Nine ladies dancing\n" +
+      "Eight maids a-milking\n" +
+      "Seven swans a-swimming\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 10
+      "On the tenth day of christmas my true love sent to me\n" +
+      "Ten lords a-leaping\n" +
+      "Nine ladies dancing\n" +
+      "Eight maids a-milking\n" +
+      "Seven swans a-swimming\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 11
+      "On the eleventh day of christmas my true love sent to me\n" +
+      "Eleven pipers piping\n" +
+      "Ten lords a-leaping\n" +
+      "Nine ladies dancing\n" +
+      "Eight maids a-milking\n" +
+      "Seven swans a-swimming\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    when 12
+      "On the twelfth day of christmas my true love sent to me\n" +
+      "Twelve drummers drumming\n" +
+      "Eleven pipers piping\n" +
+      "Ten lords a-leaping\n" +
+      "Nine ladies dancing\n" +
+      "Eight maids a-milking\n" +
+      "Seven swans a-swimming\n" +
+      "Six geese a-laying\n" +
+      "Five gold rings\n" +
+      "Four colly birds\n" +
+      "Three french hens\n" +
+      "Two turtle doves, and\n" +
+      "A partridge in a pear tree\n"
+    end
+  end
+
   def recite
-    <<~EOLYRICS
-      On the twelth day of christmas my true love sent to me
-      Twelve drummers drumming
-      Eleven pipers piping
-      Ten lords a-leaping
-      Nine ladies dancing
-      Eight maids a-milking
-      Seven swans a-swimming
-      Six geese a-laying
-      Five gold rings
-      Four colly birds
-      Three french hens
-      Two turtle doves, and
-      A partridge in a pear tree
-    EOLYRICS
+    (1..12).map {|i|
+      verse(i)
+    }.join("\n")
   end
 end
 
