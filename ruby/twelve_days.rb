1a206a447e72cc66eef56370b8bb7d85df737820
Tracy Atteberry <tracy@magicbydesign.com>
2018-03-24 15:26:13 -0500

    rubocop tweaks

--- ruby/twelve_days.rb
+++ ruby/twelve_days.rb
@@ -2,27 +2,30 @@
 
 class TwelveDays
   def recite
-    (1..12).map {|i|
+    (1..12).map do |i|
       verse(i)
-    }.join("\n")
+    end.join("\n")
   end
 
   private
 
-  ORDINALS = %w(zeroth first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth)
+  ORDINALS = %w[zeroth first second third fourth fifth sixth
+                seventh eighth ninth tenth eleventh twelfth].freeze
+
   LINES = [
-        "Twelve drummers drumming",
-        "Eleven pipers piping",
-        "Ten lords a-leaping",
-        "Nine ladies dancing",
-        "Eight maids a-milking",
-        "Seven swans a-swimming",
-        "Six geese a-laying",
-        "Five gold rings",
-        "Four colly birds",
-        "Three french hens",
-        "Two turtle doves, and",
-        "A partridge in a pear tree" ]
+    'Twelve drummers drumming',
+    'Eleven pipers piping',
+    'Ten lords a-leaping',
+    'Nine ladies dancing',
+    'Eight maids a-milking',
+    'Seven swans a-swimming',
+    'Six geese a-laying',
+    'Five gold rings',
+    'Four colly birds',
+    'Three french hens',
+    'Two turtle doves, and',
+    'A partridge in a pear tree'
+  ].freeze
 
   def verse(i)
     "On the %s day of christmas my true love sent to me\n" % ORDINALS[i] +
