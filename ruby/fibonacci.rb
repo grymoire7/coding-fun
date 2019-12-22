ef12213cda30ac9b4bc74c7cc29f399f5bcde399
Tracy Atteberry <tracy@magicbydesign.com>
2019-12-22 16:51:55 -0600

    Update for Rubocop

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -3,18 +3,20 @@
 # Fionacci calculates elemens of the Fibonacci sequence.
 #
 class Fibonacci
-  def self.upto(n)
-    return 0 if n <= 0
+  def self.upto(num)
+    return 0 if num <= 0
+
     x1, x2 = [0, 1]
-    0.upto(n - 1) { x1 += x2; x1, x2 = x2, x1 }
+    0.upto(num - 1) { x1 += x2; x1, x2 = x2, x1 }
     x1
   end
 
-  def self.array_upto(n)
+  def self.array_upto(num)
     ret = []
-    return ret if n <= 0
+    return ret if num <= 0
+
     x1, x2 = [0, 1]
-    0.upto(n - 1) { ret << x2; x1 += x2; x1, x2 = x2, x1 }
+    0.upto(num - 1) { ret << x2; x1 += x2; x1, x2 = x2, x1 }
     ret
   end
 end
