7c7e9271912fad432e1f48c5c6621055245023bd
Tracy Atteberry <tracy@magicbydesign.com>
2021-09-03 18:53:39 -0500

    Remove if..else with hash

--- ruby/trie.rb
+++ ruby/trie.rb
@@ -18,31 +18,20 @@ class Trie
   def find(key_string, node = root)
     # puts "find: node = #{.inspect}"
     # puts "find: looking for = #{key_string}"
-    if key_string.size == 1
-      # puts "find: children = #{node.children.inspect}"
-      # puts "find: key_string = #{key_string}"
-      # puts "find: key_value = #{node.children[key_string].leaf_value}"
-      return node.children[key_string].leaf_value
-    else
-      child_key = key_string[1..-1]
-      # puts "find: child_key = #{child_key}"
-      next_node = node.children[key_string[0]]
-      find(child_key, next_node)
-    end
+
+    # An amusing way to avoid the if..else
+    finders = Hash.new { |h, k| h[k] = method(:find_child) }
+    finders[1] = method(:find_leaf)
+    finders[key_string.size].call(key_string, node)
   end
 
   def add(key_string, value_string, node = root)
     # puts "add: node = #{node.inspect}"
-    if key_string.size == 1
-      leaf_node = Node.new(value_string)
-      node.children[key_string] = leaf_node
-      # puts "children = #{node.children.size}"
-      # puts "children = #{node.children}"
-    else
-      child_key = key_string[1..-1]
-      next_node = node.children[key_string[0]]
-      add(child_key, value_string, next_node)
-    end
+
+    # An amusing way to avoid the if..else
+    adders = Hash.new { |h, k| h[k] = method(:add_child) }
+    adders[1] = method(:add_leaf)
+    adders[key_string.size].call(key_string, value_string, node)
   end
 
   def to_s
@@ -58,6 +47,30 @@ class Trie
   private
 
   attr_reader :root
+
+  def add_leaf(key_string, value_string, node = root)
+    leaf_node = Node.new(value_string)
+    node.children[key_string] = leaf_node
+    # puts "children = #{node.children.size}"
+    # puts "children = #{node.children}"
+  end
+
+  def add_child(key_string, value_string, node = root)
+    child_key = key_string[1..-1]
+    next_node = node.children[key_string[0]]
+    add(child_key, value_string, next_node)
+  end
+
+  def find_leaf(key_string, node = root)
+    node.children[key_string].leaf_value
+  end
+
+  def find_child(key_string, node = root)
+    child_key = key_string[1..-1]
+    # puts "find: child_key = #{child_key}"
+    next_node = node.children[key_string[0]]
+    find(child_key, next_node)
+  end
 end
 
 RSpec.describe 'Trie' do
