1af58959aa6a1ad7f97c788b25536d6f4901ac14
Tracy Atteberry <tracy@magicbydesign.com>
2024-10-15 16:31:53 -0500

    Add contains_close_nums.rb

---
+++ ruby/contains_close_nums.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Contains close nums
+#
+# Inputs
+#   nums: an array of integers
+#   k: an integer
+#
+# Output: Boolean
+#   Returns true if two of the same value appear within k slots
+#   of each other.
+
+def contains_close_nums(nums, k)
+	return false if nums.size < 2
+
+	loc = {} # a hash mapping numbers to the index it was last found at
+  nums.each_with_index do |num, i|
+    found_at = loc[num]
+    loc[num] = i
+    unless found_at.nil?
+			return true if i - found_at <=  k
+		end
+	end
+
+	false
+end
+
+require 'rspec/autorun'
+
+RSpec.describe '#contains_close_nums' do
+  let(:k) { 3 }
+  let(:nums) { [ 2, 3, 5, 3, 37, 48, 5, 256, 2 ] }
+
+  it 'works for example 1' do
+    expect(contains_close_nums(nums, k)).to be true
+  end
+
+  it 'works for example 2' do
+    stream = [ 2, 4, 5, 3, 37, 48, 5, 256, 2 ]
+    expect(contains_close_nums(stream, 3)).to be false
+  end
+end
